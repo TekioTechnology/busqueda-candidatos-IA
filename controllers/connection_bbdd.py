d1f92b7a24e8a59d72fceb27c0049bd1da583949
Emily Herrera <e.herrera@grupoub.com>
2024-04-15 15:56:44 +0200

    h

--- controllers/connection_bbdd.py
+++ controllers/connection_bbdd.py
@@ -1,24 +1,27 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://mimika:1.Cambiame@databasecluster.ldcnr5i.mongodb.net/?retryWrites=true&w=majority&appName=DatabaseCluster"
+def establecer_conexion():
+    uri = "mongodb+srv://mimika:1.Cambiame@databasecluster.ldcnr5i.mongodb.net/?retryWrites=true&w=majority&appName=DatabaseCluster"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-    
-    # Obtener una lista de las bases de datos disponibles
-    database_names = client.list_database_names()
-
-    # Imprimir las bases de datos
-    print("Bases de datos disponibles:")
-    for db_name in database_names:
-        print(f"- {db_name}")
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Ping to your deployment. You successfully connected to MongoDB!")
         
-except Exception as e:
-    print(e)
+        # Obtain a list of available databases
+        database_names = client.list_database_names()
+
+        # Print the available databases
+        print("Available databases:")
+        for db_name in database_names:
+            print(f"- {db_name}")
+            
+    except Exception as e:
+        print(e)
+
+# Call the function to establish connection
+establecer_conexion()
--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,19 +1,47 @@
 # mi_app/controllers/main_controller.py
-from flask import Blueprint, render_template, request, current_app, send_from_directory
+from flask import Blueprint, render_template, request, current_app, send_from_directory, Response
 import os
 import json
 import spacy
 from unidecode import unidecode
 from utils.cv_utils import extraer_texto_pdf, buscar_por_palabra_clave
-
-
-
-
-
-
+# from connection_bbdd import client
 
 
 main_controller = Blueprint('main_controller', __name__)
+
+
+
+
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
+
+# uri = "mongodb+srv://mimika:1.Cambiame@databasecluster.ldcnr5i.mongodb.net/?retryWrites=true&w=majority&appName=DatabaseCluster"
+
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
+
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+    
+#     # Obtener una lista de las bases de datos disponibles
+#     database_names = client.list_database_names()
+
+#     # Imprimir las bases de datos
+#     print("Bases de datos disponibles:")
+#     for db_name in database_names:
+#         print(f"- {db_name}")
+        
+# except Exception as e:
+#     print(e)
+
+
+
+
+
+
 
 
 @main_controller.route('/subir_cv', methods=['GET', 'POST'])
@@ -22,19 +50,21 @@ def subir_cv():
         try:
             if 'cv' in request.files:
                 archivo_cv = request.files['cv']
+              
                 
                 # obtenemos el nombre original del archivo
                 nombre_archivo = archivo_cv.filename
+
                 
                 # verificamos si el archivo ya existe
-                if client.UB_cvs.pdfs.find_one({"nombre_archivo": nombre_archivo}):
+                if client.ub_cv.upload_cv.find_one({"nombre_archivo": nombre_archivo}):
                     return 'Este CV ya existe en tu base de datos.'
 
                 # convertimos el archivo a bytes
                 contenido_pdf = archivo_cv.read()
                 
                 # Guarda el archivo en la base de datos
-                client.UB_cvs.pdfs.insert_one({
+                client.ub_cv.upload_cv.pdfs.insert_one({
                     "nombre_archivo": nombre_archivo,
                     "contenido": contenido_pdf
                 })
@@ -100,8 +130,30 @@ def buscar():
 
 @main_controller.route('/descargar/<nombre_archivo>')
 def descargar(nombre_archivo):
-    return send_from_directory(os.path.join(current_app.root_path, 'uploads'), nombre_archivo, as_attachment=True)
+    pdf = client.ub_cv.upload_cv.find_one({"nombre_archivo": nombre_archivo})
+    if pdf:
+        contenido_pdf = pdf["contenido"]
+        return Response(contenido_pdf, mimetype='application/pdf', headers={'Content-Disposition': f'attachment; filename="{nombre_archivo}"'})
+    else:
+        return "El archivo no fue encontrado en la base de datos"
 
 @main_controller.route('/visualizar_pdf/<nombre_archivo>')
 def visualizar_pdf(nombre_archivo):
-    return send_from_directory(os.path.join(current_app.root_path, 'uploads'), nombre_archivo, as_attachment=False)
+    pdf = client.ub_cv.upload_cv.find_one({"nombre_archivo": nombre_archivo})
+    if pdf:
+        contenido_pdf = pdf["contenido"]
+        return Response(contenido_pdf, mimetype='application/pdf')
+    else:
+        return "El archivo no fue encontrado en la base de datos."
+
+
+
+
+
+@main_controller.route('/ver_cvs_subidos', methods=['GET'])
+def ver_cvs_subidos():
+    # Obtener todos los documentos de la colección upload_cv
+    cvs_subidos = list(client.ub_cv.upload_cv.find())
+
+    # Renderizar una plantilla para mostrar los documentos
+    return render_template('ver_cvs_subidos.html', cvs_subidos=cvs_subidos)
