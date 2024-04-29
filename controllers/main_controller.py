828a7f428529f6722aa37e12445bfd622f831f9e
Emily Herrera <e.herrera@grupoub.com>
2024-04-29 16:08:53 +0200

    corrigiendo main controller

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -2,6 +2,8 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+
+#----------------funcion de conexion con la base de datos
 def establecer_conexion():
     uri = "mongodb+srv://mimika:1.Cambiame@databasecluster.ldcnr5i.mongodb.net/?retryWrites=true&w=majority&appName=DatabaseCluster"
     client = MongoClient(uri, server_api=ServerApi('1'))
@@ -22,42 +24,8 @@ from utils.cv_utils import extraer_texto_pdf, buscar_por_palabra_clave
 
 main_controller = Blueprint('main_controller', __name__)
 
-#comprobamos la funcion de la conexion con la base de datos
-# client =establecer_conexion()
 
-
-
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-
-# uri = "mongodb+srv://mimika:1.Cambiame@databasecluster.ldcnr5i.mongodb.net/?retryWrites=true&w=majority&appName=DatabaseCluster"
-
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-    
-#     # Obtener una lista de las bases de datos disponibles
-#     database_names = client.list_database_names()
-
-#     # Imprimir las bases de datos
-#     print("Bases de datos disponibles:")
-#     for db_name in database_names:
-#         print(f"- {db_name}")
-        
-# except Exception as e:
-#     print(e)
-
-
-
-
-
-
-
-#controlador para subir los cvs a la bbdd
+#--------------controlador para subir los cvs a la bbdd
 @main_controller.route('/subir_cv', methods=['GET', 'POST'])
 def subir_cv():
     try:
@@ -89,54 +57,46 @@ def subir_cv():
 
     return render_template('formulario.html')
     
-    
+# import fitz
+# def extraer_texto_pdf(contenido_pdf):
+#     texto=""
+#     try:
+#         #abrimos el documento pdf
+#         documento=fitz.open(stream=contenido_pdf,filetype="pdf")
+#         #extrae el texto de cada pagina
+#         for pagina in documento:
+#             texto +=pagina.get_text()
+#     except Exception as e:
+#         print("error al extraer el texto pdf")
+#         return texto
 
+#funcion de buscar pdf
 @main_controller.route('/buscar', methods=['GET'])
 def mostrar_buscador():
     return render_template('buscador.html')
 
-@main_controller.route('/buscar', methods=['POST'])
-def buscar():
-    palabra_clave = request.form.get('palabra_clave', '')
-    palabra_clave_normalizada = unidecode(palabra_clave).lower()
-    resultados = []
+# @main_controller.route('/buscar', methods=['POST'])
+# def buscar():
+#     palabra_clave = request.form.get('palabra_clave', '')
+#     palabra_clave_normalizada = unidecode(palabra_clave).lower()
+#     resultados = []
 
-    db = establecer_conexion()
-    for pdf in db.upload_cv.find():
-        contenido_pdf = pdf["contenido"]
-        nombre_archivo = pdf["nombre_archivo"]
+#     db = establecer_conexion()
+#     for pdf in db.upload_cv.find():
+#         contenido_pdf = pdf["contenido"]
+#         nombre_archivo = pdf["nombre_archivo"]
 
-        # Extraer texto del PDF
-        texto_pdf = extraer_texto_pdf(contenido_pdf)
+#         # Extraer texto del PDF
+#         texto_pdf = extraer_texto_pdf(contenido_pdf)
 
-        # Buscar la palabra clave en el texto extraído
-        if palabra_clave_normalizada in unidecode(texto_pdf).lower():
-            resultados.append(nombre_archivo)
+#         # Buscar la palabra clave en el texto extraído
+#         if palabra_clave_normalizada in unidecode(texto_pdf).lower():
+#             resultados.append(nombre_archivo)
 
-    print(f'palabra clave buscada: {palabra_clave}')
-    print(f'Resultados encontrados: {resultados}')
+#     print(f'palabra clave buscada: {palabra_clave}')
+#     print(f'Resultados encontrados: {resultados}')
 
-    # Guardamos la palabra clave en un archivo JSON
-    ruta_json = os.path.join(current_app.root_path, 'mineri_text', 'data.json')
-    print(f'Ruta del archivo JSON: {ruta_json}')
-
-    # Estructura del JSON inicial
-    estructura_json = {"words": []}
-    if os.path.exists(ruta_json):
-        with open(ruta_json, 'r') as archivo_json:
-            estructura_json = json.load(archivo_json)
-
-    # Palabra clave solo si no existe en el archivo JSON
-    if palabra_clave_normalizada not in estructura_json["words"]:
-        estructura_json["words"].append(palabra_clave_normalizada)
-
-        with open(ruta_json, 'w') as archivo_json:
-            json.dump(estructura_json, archivo_json, indent=2)
-
-        print(f'Palabra clave guardada: {palabra_clave_normalizada}')
-
-    return render_template('buscador.html', palabra_clave=palabra_clave, resultados=resultados)
-
+#     return render_template('buscador.html', palabra_clave=palabra_clave, resultados=resultados)
 # @main_controller.route('/descargar/<nombre_archivo>')
 # def descargar(nombre_archivo):
 #     pdf = client.ub_cv.upload_cv.find_one({"nombre_archivo": nombre_archivo})
@@ -146,23 +106,14 @@ def buscar():
 #     else:
 #         return "El archivo no fue encontrado en la base de datos"
 
-@main_controller.route('/visualizar_pdf/<nombre_archivo>')
-def visualizar_pdf(nombre_archivo):
-    pdf = client.ub_cv.upload_cv.find_one({"nombre_archivo": nombre_archivo})
-    if pdf:
-        contenido_pdf = pdf["contenido"]
-        return Response(contenido_pdf, mimetype='application/pdf')
-    else:
-        return "El archivo no fue encontrado en la base de datos."
+# @main_controller.route('/visualizar_pdf/<nombre_archivo>')
+# def visualizar_pdf(nombre_archivo):
+#     pdf = client.ub_cv.upload_cv.find_one({"nombre_archivo": nombre_archivo})
+#     if pdf:
+#         contenido_pdf = pdf["contenido"]
+#         return Response(contenido_pdf, mimetype='application/pdf')
+#     else:
+#         return "El archivo no fue encontrado en la base de datos."
 
 
 
-
-
-# @main_controller.route('/ver_cvs_subidos', methods=['GET'])
-# def ver_cvs_subidos():
-#     # Obtener todos los documentos de la colección upload_cv
-#     cvs_subidos = list(client.ub_cv.upload_cv.find())
-
-#     # Renderizar una plantilla para mostrar los documentos
-#     return render_template('ver_cvs_subidos.html', cvs_subidos=cvs_subidos)
