74e5aaaebff9aac9b2f9b2ad52cc08ca6a042e19
TekioTechnology <tekiotechnology@gmail.com>
2025-07-07 16:31:08 +0200

    subiendo cambio muero2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from routes import register_routes
 from werkzeug.exceptions import RequestEntityTooLarge
@@ -9,17 +10,18 @@ def create_app():
     app = Flask(__name__)
     app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20 MB
 
-    @app.route("/", methods=["GET"])
-    def welcome():
-        return {"mensaje": "Bienvenido a la API de búsqueda de candidatos"}, 200
-
     @app.errorhandler(RequestEntityTooLarge)
     def handle_file_too_large(e):
         return {"error": "El archivo es demasiado grande. Límite: 20 MB"}, 413
+
+    @app.route("/", methods=["GET"])
+    def welcome():
+        return {"mensaje": "Bienvenido a la API de búsqueda de candidatos"}
 
     register_routes(app)
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    port = int(os.environ.get("PORT", 3000))  # <-- usa el puerto proporcionado por Azure
+    app.run(host="0.0.0.0", port=port, debug=True)
