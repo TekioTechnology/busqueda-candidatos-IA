bf6aae936dc17c41a76d059edc8454bf8a4cc661
TekioTechnology <tekiotechnology@gmail.com>
2025-07-07 15:44:47 +0200

    subiendo cambio muero

--- app.py
+++ app.py
@@ -9,19 +9,17 @@ def create_app():
     app = Flask(__name__)
     app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024  # 20 MB
 
-    @app.errorhandler(RequestEntityTooLarge)
-    def handle_file_too_large(e):
-        return {"error": "El archivo es demasiado grande. Límite: 20 MB"}, 413
-    
     @app.route("/", methods=["GET"])
     def welcome():
         return {"mensaje": "Bienvenido a la API de búsqueda de candidatos"}, 200
 
+    @app.errorhandler(RequestEntityTooLarge)
+    def handle_file_too_large(e):
+        return {"error": "El archivo es demasiado grande. Límite: 20 MB"}, 413
+
     register_routes(app)
     return app
 
-# Necesario para Azure (Gunicorn buscará este objeto)
-app = create_app()
-
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=3000, debug=True)
